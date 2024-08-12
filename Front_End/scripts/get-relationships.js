1c83d6e49bd1022cdecd22e77ecc08bcc5a61a6b
michaellgans <michaellgans@Victoria>
2024-08-12 14:13:40 -0500

    MLG - 2:13 PM - appends damage relations dynamically but breaks color change of underline

--- Front_End/scripts/get-relationships.js
+++ Front_End/scripts/get-relationships.js
@@ -28,10 +28,100 @@ $(document).ready(function() {
             const notEffectedBy = data.damage_relations.no_damage_from.map(p => p.name);
 
             if (superEffectiveAgainst[0] === undefined) {
-                console.log(`Nothing is supereffective against ${type}.`);
+                console.log(`${type} does double damage to nothing.`);
             } else {
                 $(".type-interaction-row").append(
                     `<h3>Super Effective Against</h3>
+                    <div class="types">
+                        <a
+                            href="/index.html"
+                            aria-label="Click to view Fairy Type interactions.">
+                            <img 
+                                src="../images/Fairy_icon_SwSh.png"
+                                alt="Fairy Type Icon"
+                            >
+                        </a>
+                    </div>
+                    `)
+            }
+
+            if (weakTo[0] === undefined) {
+                console.log(`${type} takes double damage from nothing.`);
+            } else {
+                $(".type-interaction-row").append(
+                    `<h3>Weak To</h3>
+                    <div class="types">
+                        <a
+                            href="/index.html"
+                            aria-label="Click to view Fairy Type interactions.">
+                            <img 
+                                src="../images/Fairy_icon_SwSh.png"
+                                alt="Fairy Type Icon"
+                            >
+                        </a>
+                    </div>
+                    `)
+            }
+
+            if (notEffectiveAgainst[0] === undefined) {
+                console.log(`${type} does half damage to nothing.`);
+            } else {
+                $(".type-interaction-row").append(
+                    `<h3>Not Very Effective Against</h3>
+                    <div class="types">
+                        <a
+                            href="/index.html"
+                            aria-label="Click to view Fairy Type interactions.">
+                            <img 
+                                src="../images/Fairy_icon_SwSh.png"
+                                alt="Fairy Type Icon"
+                            >
+                        </a>
+                    </div>
+                    `)
+            }
+
+            if (resistantTo[0] === undefined) {
+                console.log(`${type} takes half damage from nothing.`);
+            } else {
+                $(".type-interaction-row").append(
+                    `<h3>Resistant To</h3>
+                    <div class="types">
+                        <a
+                            href="/index.html"
+                            aria-label="Click to view Fairy Type interactions.">
+                            <img 
+                                src="../images/Fairy_icon_SwSh.png"
+                                alt="Fairy Type Icon"
+                            >
+                        </a>
+                    </div>
+                    `)
+            }
+
+            if (noEffectAgainst[0] === undefined) {
+                console.log(`Nothing is immune to ${type}.`);
+            } else {
+                $(".type-interaction-row").append(
+                    `<h3>Has No Effect Against</h3>
+                    <div class="types">
+                        <a
+                            href="/index.html"
+                            aria-label="Click to view Fairy Type interactions.">
+                            <img 
+                                src="../images/Fairy_icon_SwSh.png"
+                                alt="Fairy Type Icon"
+                            >
+                        </a>
+                    </div>
+                    `)
+            }
+
+            if (notEffectedBy[0] === undefined) {
+                console.log(`${type} is immune to nothing.`);
+            } else {
+                $(".type-interaction-row").append(
+                    `<h3>Not Effected By</h3>
                     <div class="types">
                         <a
                             href="/index.html"
