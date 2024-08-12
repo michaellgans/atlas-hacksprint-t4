189d3a5b49b9f41246027e3daeabeb94e42c4bab
michaellgans <michaellgans@Victoria>
2024-08-12 10:45:00 -0500

    MLG - 10:44 AM - correctly changes title of single-type.html depending on user click

--- Front_End/scripts/change-color.js
+++ Front_End/scripts/change-color.js
@@ -1,10 +1,26 @@
 // Changes the style of the page based on the color selected by the user
 
+import capWordUtil from "../../Utils/Helper_Functions/capWordUtil.js";
+
 $(document).ready(function() {
     /* Retrieves the color from URL params */
     const params = new URLSearchParams(window.location.search);
-    const type = params.get('type');
+    let type = params.get('type');
 
     console.log(`This should be the pokemon type: ${type}.`);
+
+    /* Change Title Text */
+    const regex = /([^-]*){1}-/;
+    const match = type.match(regex);
+    type = match[1];
+    type = capWordUtil(type, " ");
+
+    $("#type-name").text(type);
+
+    /* Change Icon */
+
+    /* Change Background */
+
+    /* Change Underline */
     
 });
