ca1ab4317647cd6c4cbfc18c9d68b0d432ca4e27
michaellgans <michaellgans@Victoria>
2024-08-12 18:25:33 -0500

    MLG - 6:25 PM - troubleshooting dynamically generated elements

--- Front_End/scripts/get-relationships.js
+++ Front_End/scripts/get-relationships.js
@@ -38,8 +38,9 @@ $(document).ready(function() {
                     const upperType = capWordUtil(superEffectiveAgainst[x], " ");
                     interactionItem += 
                         `<a
-                            href="http://localhost:5500/Front_End/static/pages/single-type.html?type=${type}-type"
-                            aria-label="Click to view ${upperType} Type interactions.">
+                            href="http://localhost:5500/Front_End/static/pages/single-type.html?type=${superEffectiveAgainst[x]}-type"
+                            aria-label="Click to view ${upperType} Type interactions."
+                        >
                             <img 
                                 src="../images/${upperType}_icon_SwSh.png"
                                 alt="${upperType} Type Icon"
@@ -53,98 +54,118 @@ $(document).ready(function() {
             if (weakTo[0] === undefined) {
                 console.log(`${type} takes double damage from nothing.`);
             } else {
-                $(".type-interaction-row").append(
-                    `<h3>Weak To</h3>
-                    <div class="types">
-                        <a
-                            href="/index.html"
-                            aria-label="Click to view Fairy Type interactions.">
+                let interactionItem = `
+                    <h3>Weak To</h3>
+                    <div class="types">`
+                for (let x = 0; x < weakTo.length; x++) {
+                    const upperType = capWordUtil(weakTo[x], " ");
+                    interactionItem += 
+                        `<a
+                            href="http://localhost:5500/Front_End/static/pages/single-type.html?type=${weakTo[x]}-type"
+                            aria-label="Click to view ${upperType} Type interactions.">
                             <img 
-                                src="../images/Fairy_icon_SwSh.png"
-                                alt="Fairy Type Icon"
+                                src="../images/${upperType}_icon_SwSh.png"
+                                alt="${upperType} Type Icon"
                             >
-                        </a>
-                    </div>
-                    `)
+                        </a>`
+                }
+                interactionItem += `</div>`
+                $(".type-interaction-row").append(interactionItem);
             }
 
             if (notEffectiveAgainst[0] === undefined) {
                 console.log(`${type} does half damage to nothing.`);
             } else {
-                $(".type-interaction-row").append(
-                    `<h3>Not Very Effective Against</h3>
-                    <div class="types">
-                        <a
-                            href="/index.html"
-                            aria-label="Click to view Fairy Type interactions.">
+                let interactionItem = `
+                    <h3>Not Very Effective Against</h3>
+                    <div class="types">`
+                for (let x = 0; x < notEffectiveAgainst.length; x++) {
+                    const upperType = capWordUtil(notEffectiveAgainst[x], " ");
+                    interactionItem += 
+                        `<a
+                            href="http://localhost:5500/Front_End/static/pages/single-type.html?type=${notEffectiveAgainst[x]}-type"
+                            aria-label="Click to view ${upperType} Type interactions.">
                             <img 
-                                src="../images/Fairy_icon_SwSh.png"
-                                alt="Fairy Type Icon"
+                                src="../images/${upperType}_icon_SwSh.png"
+                                alt="${upperType} Type Icon"
                             >
-                        </a>
-                    </div>
-                    `)
+                        </a>`
+                }
+                interactionItem += `</div>`
+                $(".type-interaction-row").append(interactionItem);
             }
 
             if (resistantTo[0] === undefined) {
                 console.log(`${type} takes half damage from nothing.`);
             } else {
-                $(".type-interaction-row").append(
-                    `<h3>Resistant To</h3>
-                    <div class="types">
-                        <a
-                            href="/index.html"
-                            aria-label="Click to view Fairy Type interactions.">
+                let interactionItem = `
+                    <h3>Resistant To</h3>
+                    <div class="types">`
+                for (let x = 0; x < resistantTo.length; x++) {
+                    const upperType = capWordUtil(resistantTo[x], " ");
+                    interactionItem += 
+                        `<a
+                            href="http://localhost:5500/Front_End/static/pages/single-type.html?type=${resistantTo[x]}-type"
+                            aria-label="Click to view ${upperType} Type interactions.">
                             <img 
-                                src="../images/Fairy_icon_SwSh.png"
-                                alt="Fairy Type Icon"
+                                src="../images/${upperType}_icon_SwSh.png"
+                                alt="${upperType} Type Icon"
                             >
-                        </a>
-                    </div>
-                    `)
+                        </a>`
+                }
+                interactionItem += `</div>`
+                $(".type-interaction-row").append(interactionItem);
             }
 
             if (noEffectAgainst[0] === undefined) {
                 console.log(`Nothing is immune to ${type}.`);
             } else {
-                $(".type-interaction-row").append(
-                    `<h3>Has No Effect Against</h3>
-                    <div class="types">
-                        <a
-                            href="/index.html"
-                            aria-label="Click to view Fairy Type interactions.">
+                let interactionItem = `
+                    <h3>Has No Effect Against</h3>
+                    <div class="types">`
+                for (let x = 0; x < noEffectAgainst.length; x++) {
+                    const upperType = capWordUtil(noEffectAgainst[x], " ");
+                    interactionItem += 
+                        `<a
+                            href="http://localhost:5500/Front_End/static/pages/single-type.html?type=${noEffectAgainst[x]}-type"
+                            aria-label="Click to view ${upperType} Type interactions.">
                             <img 
-                                src="../images/Fairy_icon_SwSh.png"
-                                alt="Fairy Type Icon"
+                                src="../images/${upperType}_icon_SwSh.png"
+                                alt="${upperType} Type Icon"
                             >
-                        </a>
-                    </div>
-                    `)
+                        </a>`
+                }
+                interactionItem += `</div>`
+                $(".type-interaction-row").append(interactionItem);
             }
 
             if (notEffectedBy[0] === undefined) {
                 console.log(`${type} is immune to nothing.`);
             } else {
-                $(".type-interaction-row").append(
-                    `<h3>Not Effected By</h3>
-                    <div class="types">
-                        <a
-                            href="/index.html"
-                            aria-label="Click to view Fairy Type interactions.">
+                let interactionItem = `
+                    <h3>Not Effected By</h3>
+                    <div class="types">`
+                for (let x = 0; x < notEffectedBy.length; x++) {
+                    const upperType = capWordUtil(notEffectedBy[x], " ");
+                    interactionItem += 
+                        `<a
+                            href="http://localhost:5500/Front_End/static/pages/single-type.html?type=${notEffectedBy[x]}-type"
+                            aria-label="Click to view ${upperType} Type interactions.">
                             <img 
-                                src="../images/Fairy_icon_SwSh.png"
-                                alt="Fairy Type Icon"
+                                src="../images/${upperType}_icon_SwSh.png"
+                                alt="${upperType} Type Icon"
                             >
-                        </a>
-                    </div>
-                    `)
+                        </a>`
+                }
+                interactionItem += `</div>`
+                $(".type-interaction-row").append(interactionItem);
             }
 
             /* Change Underline Color after element is created*/
             $("h3").css({"border-bottom": `solid 7px var(--${type}-type-65)`});
         },
         error: function() {
-            console.log("Get Pokemon Error");
+            console.log("Get Type Interaction Error");
         }
     });
 });
