093e9cd5eb0ef33b4c0dbc36db74378a85201a1c
michaellgans <michaellgans@Victoria>
2024-08-14 13:08:24 -0500

    MLG - 1:08 PM - dynamically generates a list of pokemon based on type

--- Front_End/scripts/get-pokemon.js
+++ Front_End/scripts/get-pokemon.js
@@ -12,118 +12,92 @@ $(document).ready(function() {
     const regex = /([^-]*){1}-/;
     const match = type.match(regex);
     let typeInput = match[1];
-    // const upperType = capWordUtil(type, " ");
 
-    // Pull pokemon names
-    function getPokemonNames(typeInput) {
+    // Pull pokemon ids and sprites
+    function getPokemonInfo(typeInput) {
         $.ajax({
             // API Entry Point
             // url: `https://pokeapi.co/api/v2/type/${typeInput}`,
-            url: `http://localhost:5000/types/${typeInput}`,
+            url: `http://localhost:5000/${typeInput}/pokemon`,
             method: "GET",
             dataType: "json",
             success: function(data) {
-                // Traverse through the parsed data
-                const pokemonList = data.pokemonList;
-                // Call function that gets info per pokemon name
-                getPokemonInfo(pokemonList);
+                data.forEach( pokemon => {
+                // Pull ID, Name, Type1, and Sprite
+                const id = pokemon._id;
+                const name = capWordUtil(pokemon.pokeName, " ");
+                const type1 = capWordUtil(pokemon.pokeType1, " ");
+                const sprite = pokemon.pokeSprite;
+
+                // Create a name in HTML
+                let pokemonItem = `
+                <div class="pokemon-item d-flex flex-row align-items-center">
+                    <img
+                        id="pokeball-sprite"
+                        class="px-2"
+                        src="../images/Bag_Poké_Ball_SV_Sprite.png"
+                        alt="Open pokedex modal"
+                        data-toggle="modal"
+                        data-target="#pokedex${id}"
+                    >
+                    <text id="pokemon-id">#${id}</text>
+                    <text id="pokemon-name">${name}</text>
+                    <!-- Modal -->
+                    <div class="modal fade" id="pokedex${id}" tabindex="-1" role="dialog" aria-labelledby="Pokedex showing ${name}." aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered" role="document">
+                            <div class="modal-content">
+                                <div class="modal-body d-flex justify-content-center">
+                                    <div class="pokedex-screen d-flex flex-column">
+                                        <div class="pokemon-name">${name}</div>
+                                        <div class="pokemon-id">#${id}</div>
+                                        <div class="pokemon-sprite">
+                                            <img
+                                                id="sprite"
+                                                src="${sprite}"
+                                                alt="Picture of ${name}"
+                                            >
+                                        </div>
+                                        <div class="types-container d-flex flex-row align-items-center">
+                                            <div class="type1">
+                                                <img
+                                                    id=type1
+                                                    src="../images/${type1}_icon_SwSh.png"
+                                                    alt="${type1} Type Icon"
+                                                >
+                                            </div>`
+
+                // Check for Type2
+                if (pokemon.pokeType2 !== null) {
+                    let type2 = capWordUtil(pokemon.pokeType2, " ");
+                    let type2Element = `                                            
+                        <div class="type2">
+                            <img
+                                id=type2
+                                src="../images/${type2}_icon_SwSh.png"
+                                alt="${type2} Type Icon"
+                            >
+                        </div>`
+                    pokemonItem +=  type2Element;
+                } else {
+                    console.log(`${name} has no type2`);
+                }
+
+                pokemonItem += `</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>`;
+
+                const populateList = $(".pokemon-list-container");
+                populateList.append(pokemonItem);
+                });
             },
             error: function() {
                 console.log("Get Pokemon Error");
             }
         });
     }
-
-    // Pull pokemon ids and sprites
-    function getPokemonInfo(pokemonList) {
-        pokemonList.forEach(pokemonName => {
-            $.ajax({
-                // API Entry Point
-                // url: `https://pokeapi.co/api/v2/type/${typeInput}`,
-                url: `http://http://localhost:5000/pokemon/${pokemonName}`,
-                method: "GET",
-                dataType: "json",
-                success: function(data) {
-                    const pokemonId = data._id;
-                    const pokemonSprite = data.pokeSprite;
-                    const type1 = pokeType1;
-                    const type2 = pokeType2;
-                },
-                error: function() {
-                    console.log("Get Pokemon Error");
-                }
-            });
-        });
-    }
-
-    // function displayResults(pokemonList, pokemonData) {
-    //     const resultsDiv = $(".pokemon-list-container");
-    //     // Make sure it's empty
-    //     resultsDiv.empty();
-    //     // If there is data...
-    //     if (pokemonList.length > 0) {
-    //         // For each pokemon found
-    //         pokemonList.forEach(pokemon => {
-    //             // Create a name in HTML
-    //             let upperCase = capWordUtil(pokemon, " ");
-    //             getPokemonInfo(pokemon);
-    //             const pokemonItem = `
-    //             <div class="pokemon-item d-flex flex-row align-items-center">
-    //                 <img
-    //                     id="pokeball-sprite"
-    //                     class="px-2"
-    //                     src="../images/Bag_Poké_Ball_SV_Sprite.png"
-    //                     alt="Open pokedex modal"
-    //                     data-toggle="modal"
-    //                     data-target="#pokedex1"
-    //                 >
-    //                 <text id="pokemon-id">#001</text>
-    //                 <text id="pokemon-name">${upperCase}</text>
-    //                 <!-- Modal -->
-    //                 <div class="modal fade" id="pokedex1" tabindex="-1" role="dialog" aria-labelledby="Pokedex showing the pokemon that was clicked on." aria-hidden="true">
-    //                     <div class="modal-dialog modal-dialog-centered" role="document">
-    //                         <div class="modal-content">
-    //                             <div class="modal-body d-flex justify-content-center">
-    //                                 <div class="pokedex-screen d-flex flex-column">
-    //                                     <div class="pokemon-name">${upperCase}</div>
-    //                                     <div class="pokemon-id">#001</div>
-    //                                     <div class="pokemon-sprite">
-    //                                         <img
-    //                                             id="sprite"
-    //                                             src="../images/test-sprite.png"
-    //                                             alt="Picture of ${upperCase}"
-    //                                         >
-    //                                     </div>
-    //                                     <div class="types-container d-flex flex-row align-items-center">
-    //                                         <div class="type1">
-    //                                             <img
-    //                                                 id=type1
-    //                                                 src="../images/Poison_icon_SwSh.png"
-    //                                                 alt="Poison Type Icon"
-    //                                             >
-    //                                         </div>
-    //                                         <div class="type2">
-    //                                             <img
-    //                                                 id=type2
-    //                                                 src="../images/Grass_icon_SwSh.png"
-    //                                                 alt="Grass Type Icon"
-    //                                             >
-    //                                         </div>
-    //                                     </div>
-    //                                 </div>
-    //                             </div>
-    //                         </div>
-    //                     </div>
-    //                 </div>
-    //             </div>
-    //             `;
-    //             // Append as a new object to HTML
-    //             resultsDiv.append(pokemonItem);
-    //         })
-    //     } else {
-    //         resultsDiv.append("<div>No Pokemon of That Type!</div>");
-    //     }
-    // }
-
-    // getPokemonNames(typeInput);
+    getPokemonInfo(typeInput);
 });
